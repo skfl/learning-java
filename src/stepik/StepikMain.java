338c897d206d9b64d3e20c6eb1305829edeb0b57
skyfall <edik.rogachkov@gmail.com>
2021-08-23 20:10:29 +0300

    moving to other host

--- src/stepik/StepikMain.java
+++ src/stepik/StepikMain.java
@@ -1,12 +1,35 @@
 package stepik;
 
-import stepik.Palindrome.IsPalindrome;
-import stepik.factorial.Factorial;
-import stepik.types.IsPowerOfTwo;
+import com.sun.source.tree.NewArrayTree;
+import stepik.mergeSort.MergeSort;
+import stepik.mergeSort.MergeSort2;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class StepikMain {
     public static void main(String[] args) {
-        Factorial.factorial(10);
 
+
+        Random rndm = new Random();
+        int[] rnd1 = new int[5];
+        for(int i=0;i<rnd1.length;i++){
+            rnd1[i] = rndm.nextInt(10);
+        }
+        int[] rnd2 = new int[5];
+        for(int i=0;i<rnd2.length;i++){
+            rnd2[i] = rndm.nextInt(10);
+        }
+/*        int[] rnd1 = new int[]{1, 1, 7, 7, 8};
+        int[] rnd2 = new int[]{1, 2, 3, 5, 7};*/
+
+
+        Arrays.sort(rnd1);
+        Arrays.sort(rnd2);
+        System.out.println(Arrays.toString(rnd1));
+        System.out.println(Arrays.toString(rnd2));
+        int res[];
+        res = MergeSort2.mergeSort(rnd1, rnd2);
+        System.out.println(Arrays.toString(res));
     }
 }
--- src/stepik/mergeSort/MergeSort.java
+++ src/stepik/mergeSort/MergeSort.java
@@ -1,8 +1,74 @@
 package stepik.mergeSort;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static int[] mergeArrays(int[] a1, int[] a2) {
+        int[] res = new int[a1.length + a2.length];
+        int[] tmp = new int[1];
+        int minArr = 0;
+        int maxArr = 0;
+        int pos = 0;
 
-        return a1;
+        if(a1[0]<a2[0]){
+            res[pos]=a1[0];
+            pos+=1;
+            tmp[0]=a2[0];
+        }else if(a1[0]>a2[0]){
+            res[pos]=a2[0];
+            pos+=1;
+            tmp[0]=a2[0];
+        }else {
+            res[pos]=a1[0];
+            pos+=1;
+            tmp[0]=a2[0];
+        }
+
+        for (int i = 1; i < res.length; i++) {
+
+            if(a1[i]<a2[i] || a1[i]==a2[i]){
+                minArr = a1[i];
+                maxArr = a2[i];
+            }else if(a1[i]>a2[i] && a1[i]!=a2[i]){
+                minArr = a2[i];
+                maxArr = a1[i];
+            }
+
+            if(minArr<tmp[0]){
+                res[pos]=minArr;
+                pos+=1;
+                if(maxArr<tmp[0] || maxArr==tmp[0]){
+                    res[pos]=maxArr;
+                    pos+=1;
+                }else {
+                    res[pos]=tmp[0];
+                    pos+=1;
+                    tmp[0]=maxArr;
+                }
+            }
+
+            if(tmp[0]<minArr){
+                res[pos]=tmp[0];
+                pos+=1;
+                res[pos]=minArr;
+                tmp[0]=maxArr;
+            }
+
+            if(tmp[0] == minArr){
+                res[pos]=tmp[0];
+                pos+=1;
+                res[pos]=minArr;
+                tmp[0]=maxArr;
+            }
+
+            System.out.println(Arrays.toString(res));
+        }
+        return res;
     }
 }
+
+
+/*
+   int[] a1 = new int[] {21, 23, 24, 40, 75, 76, 78, 77, 900, 2100, 2200, 2300, 2400, 2500};
+   int[] a2 = new int[] {10, 11, 41, 50, 65, 86, 98, 101, 190, 1100, 1200, 3000, 5000};
+ */
